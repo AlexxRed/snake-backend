241fab427247d3f6e03865cc57d81d6e1efc159d
Alexander Krasnobokyi <87702429+AlexxRed@users.noreply.github.com>
2022-12-07 23:36:13 +0200

    started

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+
+
+const dotenv = require('dotenv');
+dotenv.config()
+
+// const authRouter = require('./routes/api/auth')
+
+const app = express();
+
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api/auth", authRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, req, res, next) => {
+  if (err.status !== 500) {
+  res.status(err.status).json({ message: err.message })
+}
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
