4b5c4024da3687718f01162e921980ad0635b12e
Alexander Krasnobokyi <87702429+AlexxRed@users.noreply.github.com>
2022-12-08 16:08:04 +0200

    refactoring

--- app.js
+++ app.js
@@ -21,18 +21,14 @@ app.use(express.json());
 app.use("/api/top", topPlayersRouter);
 
 app.use((req, res) => {
-    res.send('hello its work')
-  });
-
-app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+    res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
-  if (err.status !== 500) {
-  res.status(err.status).json({ message: err.message })
+    if (err.status !== 500) {
+    res.status(err.status).json({ message: err.message })
 }
-  res.status(500).json({ message: err.message });
+    res.status(500).json({ message: err.message });
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const { DB_HOST } = process.env
 
 mongoose.set('strictQuery', true);
 
-
 mongoose.connect(DB_HOST)
-  .then(()=>console.log("Database connection successful"))
-  .catch(error => { 
+    .then(()=>console.log("Database connection successful"))
+    .catch(error => { 
     console.log(error.message);
     process.exit(1)
-  })
+})
 
 app.listen((process.env.PORT || 5000), () => {
     console.log("Server running. Use our API on port: 5000")
